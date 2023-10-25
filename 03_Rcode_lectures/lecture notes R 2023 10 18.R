8774bcf0aaf618c7d92f8044b42bad1d46f4e311
ajrtallman <62300107+ajrtallman@users.noreply.github.com>
2023-10-25 10:24:10 +0300

    added textbooks and slides

---
+++ 03_Rcode_lectures/lecture notes R 2023 10 18.R
@@ -0,0 +1,59 @@
+
+#I am doing math
+14+100
+12*4
+15/3
+#4^2
+31%%7
+31%/%7
+31/7
+1:20
+21:40
+
+7*4+3
+7*(4+3)
+
+pi
+
+LETTERS
+letters
+
+##Assigning a constant
+
+a = 5.678
+a <- 5.678
+
+4*a
+
+##Make a list
+
+codes <- c("A", "M", "S", "O", "F", "T", "D")  
+
+awake <- c(9, 9, 10, 5, 6, 7, 6)
+
+codes
+
+awake
+
+##Functions 
+#Sum 
+sum(awake)
+#Mean
+sum(awake)/7
+mean(awake)
+
+coffee <- c("y", "n", "n", "y", "y", "y", "n")
+
+codes
+awake
+coffee
+
+df1 <- rbind(codes, awake, coffee)
+
+df1
+
+numbers <- c(1,3,5)
+
+df1 <- rbind(codes, awake, coffee)
+df2 <- cbind(codes, awake, coffee)
+df2
